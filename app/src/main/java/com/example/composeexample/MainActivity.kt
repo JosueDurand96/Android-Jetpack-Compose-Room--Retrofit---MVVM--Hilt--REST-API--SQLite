602d75d1bacb2286280d4ebe0d0f6778b9aa40ed
Josué Durand Pebe <josue.durand8@gmail.com>
2022-01-15 15:35:25 -0500

    compose

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.composeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.composeexample.model.User
 import com.example.composeexample.ui.theme.ComposeExampleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             ComposeExampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Girl")
+                    MyAppPrincipal()
                 }
             }
         }
@@ -29,21 +32,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String,
-             viewModel: UserViewModel = hiltViewModel()
-
+fun MyAppPrincipal(
+    viewModel: UserViewModel = hiltViewModel()
 ) {
-    Button(onClick = {
-       // viewModel.getUser()
-    }){
+    val users by viewModel.users.observeAsState(arrayListOf())
+    val isLoading by viewModel.isLoading.observeAsState(false)
+
+    MyApp(onAddClick = {
+        viewModel.addUser()
+    },
+        users = users,
+        isLoading = isLoading
+    )
+}
+
+@Composable
+fun MyApp(
+    onAddClick: (() -> Unit)? = null,
+    users: List<User>,
+    isLoading: Boolean
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Simple Rest  + Room") },
+                actions = {
+                    IconButton(onClick = {
+                        onAddClick?.invoke()
+                    }) {
+                        Icon(Icons.Filled.Add, "Add")
+                    }
+                }
+            )
+        }
+    ) {
+        LazyColumn{
+            var itemCount = users.size
+            if (isLoading) itemCount++
+
+            items(count = itemCount) { index ->
+                var auxIndex = index
+                if (isLoading) {
+                    if (auxIndex == 0 )
+                        return@items Text(text = "IsLoading")
+                    auxIndex--
+                }
+            }
+        }
 
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeExampleTheme {
-        Greeting("Android")
+        MyApp(onAddClick = null, users = listOf(), isLoading = false)
     }
 }
