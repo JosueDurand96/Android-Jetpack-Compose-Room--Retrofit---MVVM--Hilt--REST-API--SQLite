3c54f853e2dd7bc784de65a9b1327bcb9661bd2f
Josué Durand Pebe <josue.durand8@gmail.com>
2022-01-15 16:43:34 -0500

    finish

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,15 +12,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberImagePainter
 import com.example.composeexample.model.User
 import com.example.composeexample.ui.theme.ComposeExampleTheme
 import com.valentinilk.shimmer.shimmer
@@ -47,9 +52,13 @@ fun MyAppPrincipal(
     val users by viewModel.users.observeAsState(arrayListOf())
     val isLoading by viewModel.isLoading.observeAsState(false)
 
-    MyApp(onAddClick = {
-        viewModel.addUser()
-    },
+    MyApp(
+        onAddClick = {
+            viewModel.addUser()
+        },
+        onDeleteClick = {
+            viewModel.deleteUser(it)
+        },
         users = users,
         isLoading = isLoading
     )
@@ -58,13 +67,14 @@ fun MyAppPrincipal(
 @Composable
 fun MyApp(
     onAddClick: (() -> Unit)? = null,
+    onDeleteClick: ((toDelete :User) -> Unit)? = null,
     users: List<User>,
     isLoading: Boolean
 ) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Simple Rest  + Room") },
+                title = { Text(text = "Use to Room + hilt+ MVVM ") },
                 actions = {
                     IconButton(onClick = {
                         onAddClick?.invoke()
@@ -75,16 +85,51 @@ fun MyApp(
             )
         }
     ) {
-        LazyColumn{
+        LazyColumn {
             var itemCount = users.size
             if (isLoading) itemCount++
 
             items(count = itemCount) { index ->
                 var auxIndex = index
                 if (isLoading) {
-                    if (auxIndex == 0 )
+                    if (auxIndex == 0)
                         return@items LoadingCard()
                     auxIndex--
+                }
+                val user = users[auxIndex]
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    elevation = 1.dp,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .fillMaxWidth()
+                ) {
+                    Row(modifier = Modifier.padding(8.dp)) {
+                        Image(
+                            modifier = Modifier.size(50.dp),
+                            painter = rememberImagePainter(
+                                data = user.thumbnail,
+                                builder = {
+                                    placeholder(R.drawable.ic_launcher_background)
+                                }
+                            ),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillHeight
+                        )
+                        Spacer()
+                        Column(
+                            Modifier.weight(1f)
+                        ) {
+                            Text("${user.name} ${user.lastName}")
+                            Text(user.city)
+                        }
+                        Spacer()
+                        IconButton(onClick = {
+                            onDeleteClick?.invoke(user)
+                        }) {
+                            Icon(Icons.Filled.Delete, "Remove")
+                        }
+                    }
                 }
             }
         }
@@ -94,15 +139,15 @@ fun MyApp(
 
 
 @Composable
-fun LoadingCard(){
-   Card(
-       shape = RoundedCornerShape(8.dp),
-       elevation = 1.dp,
-       modifier = Modifier
-           .padding(horizontal = 8.dp, vertical = 4.dp)
-           .fillMaxWidth()
-           .testTag("loadingCard")
-   ) {
+fun LoadingCard() {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 1.dp,
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .fillMaxWidth()
+            .testTag("loadingCard")
+    ) {
         Row(modifier = Modifier.padding(8.dp)) {
             ImageLoading()
             Spacer()
@@ -127,11 +172,11 @@ fun LoadingCard(){
                 }
             }
         }
-   }
+    }
 }
 
 @Composable
-fun ImageLoading(){
+fun ImageLoading() {
     Box(modifier = Modifier.shimmer()) {
         Box(
             modifier = Modifier
@@ -143,7 +188,6 @@ fun ImageLoading(){
 
 @Composable
 fun Spacer(size: Int = 8) = Spacer(modifier = Modifier.size(size.dp))
-
 
 
 @Preview(showBackground = true)
